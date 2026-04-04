8823708d3e0af4a14759373d92695f078624d49d
Manuel Pavez <manupavez22@gmail.com>
2026-04-04 13:47:38 -0300

    jhj

--- supabase/functions/send-lead-email/index.ts
+++ supabase/functions/send-lead-email/index.ts
@@ -5,16 +5,17 @@ import { serve } from "https://deno.land/std/http/server.ts";
 serve(async (req) => {
   const { name, email, level, goal, message } = await req.json();
 
-  const RESEND_API_KEY = Deno.env.get("re_KuHc2Mg4_JBT1FjZvGc242m6PeQtqgc6g");
+const RESEND_API_KEY = Deno.env.get("RESEND_API_KEY");
+console.log("KEY:", RESEND_API_KEY);
 
   const res = await fetch("https://api.resend.com/emails", {
     method: "POST",
     headers: {
-      "Authorization": `Bearer ${RESEND_API_KEY}`,
+"Authorization": `Bearer ${RESEND_API_KEY}`,
       "Content-Type": "application/json"
     },
     body: JSON.stringify({
-      from: "Frequency Lab <onboarding@resend.dev>",
+      from: "onboarding@resend.dev",
       to: ["manupavez22@gmail.com"],
       subject: "Nuevo lead",
       html: `
