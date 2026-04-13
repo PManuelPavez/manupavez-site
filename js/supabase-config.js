b70f978658952c04a70fbd5e2174f9cfe6f5fb6c
Manuel Pavez <manupavez22@gmail.com>
2026-04-13 20:37:07 -0300

    backend fix

--- js/supabase-config.js
+++ js/supabase-config.js
@@ -1,16 +1,8 @@
-
 import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm';
 
-// 🔹 tus datos
 const supabaseUrl = "https://psnprhzowknhfylvgcci.supabase.co";
-const supabaseKey = "  anonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBzbnByaHpvd2tuaGZ5bHZnY2NpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjYxNTM5MDQsImV4cCI6MjA4MTcyOTkwNH0.FFGPhYc_8J-U5BSvx0VGnpzmaGLoP-NX-6MRe0RMR0U",
-";
+const supabaseKey = "TU_ANON_KEY";
 
-// 🔥 CREAR CLIENTE REAL
 window.supabase = createClient(supabaseUrl, supabaseKey);
 
-// (opcional) mantener tu objeto si lo usás en otro lado
-window.MP_SUPABASE = {
-  url: supabaseUrl,
-  anonKey: supabaseKey
-};
+console.log("SUPABASE OK", window.supabase);
